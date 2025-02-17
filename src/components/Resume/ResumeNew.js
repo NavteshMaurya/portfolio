558783e53b29a2d4608a0f96f43457c8c31e5414
Navtesh <navteshmaurya@gmail.com>
2025-02-17 12:24:04 +0530

    sixth commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,47 +1,38 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Container, Row,Col } from "react-bootstrap";
+import React, { useState, useEffect, useRef } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/Navtesh_cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+
+
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-  const [containerWidth, setContainerWidth] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(800);
   const containerRef = useRef(null);
 
   useEffect(() => {
-    setWidth(window.innerWidth);
+    const updateWidth = () => {
+      if (containerRef.current) {
+        const newWidth = containerRef.current.offsetWidth;
+        setContainerWidth(newWidth > 400 ? newWidth : 400); // Ensuring min width of 400px
+      }
+    };
+
+    updateWidth();
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
   }, []);
-
-  const ResponsivePDF = ({ pdf }) => {
-   
-  
-    useEffect(() => {
-      const updateWidth = () => {
-        if (containerRef.current) {
-          setContainerWidth(containerRef.current.offsetWidth);
-        }
-      };
-  
-      updateWidth();
-      window.addEventListener('resize', updateWidth);
-      return () => window.removeEventListener('resize', updateWidth);
-    }, []);
-  
-
-  }
-
-
 
   return (
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Row className="justify-content-center" style={{ position: "relative" }}>
           <Button
             variant="primary"
             href={pdf}
@@ -52,16 +43,21 @@ function ResumeNew() {
             &nbsp;Download CV
           </Button>
         </Row>
+        
         <Row className="resume justify-content-center">
-        <Col xs={12} md={10} lg={8} className="px-3" ref={containerRef}>
-          <Document file={pdf} className="d-flex flex-column justify-content-center align-items-center">
-            <Page pageNumber={1} width={containerWidth} />
-            <Page pageNumber={2} width={containerWidth} />
-            <Page pageNumber={3} width={containerWidth} />
-          </Document>
-        </Col>
-      </Row>
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Col xs={12} md={10} lg={8} className="px-3" ref={containerRef}>
+            <Document 
+              file={pdf} 
+              className="d-flex flex-column justify-content-center align-items-center"
+            >
+              <Page pageNumber={1} width={containerWidth} />
+              <Page pageNumber={2} width={containerWidth} />
+              <Page pageNumber={3} width={containerWidth} />
+            </Document>
+          </Col>
+        </Row>
+
+        <Row className="justify-content-center" style={{ position: "relative" }}>
           <Button
             variant="primary"
             href={pdf}
@@ -77,4 +73,4 @@ function ResumeNew() {
   );
 }
 
-export default ResumeNew;
+export default ResumeNew
